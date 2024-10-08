59005e2d6b52938087f0d3c01845a8bf6a3bda03
Belwen <sarah.himeur@gmail.com>
2024-10-08 18:28:29 +0200

    notebook ingest

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -1,3 +1,11 @@
+import pytest
+
+def test_ocr():
+
+
+def test_clean():
+    clean_url = "http://clean:8903/clean" # url de l'OCR
+    
 
 txt_test = """
 FACTURE
@@ -56,5 +64,5 @@ SWIFT/BIC: ABCDFRP1XxX
 """
 
 
-result = "factur joanna binet 48 coubertin 31400 pari envov numéro fr.001 cendrilon ayot date 2901/2019 69 rue natier 46 st fertéol 22000 92360 ile-de-france command esoa0t9 echeance 24/08/2019 ae designation prix unir ht montant grand brun escargot manger 4100.00 100.00 petit mariniére uniforme bleu 15.00 30.00 facile jouer accordéon 5.00 total 145.00 20.0 29.00 174.006 condition modalite paiement d0 15 jour caisse epargne iban fr12 1234 5678 swiftibic abcdfrp1xx facturer envoyer cendritlon ferréol lle-de-france are 1 . modalité dd swift / bic abcdfrp1xxx"
+result_test = "factur joanna binet 48 coubertin 31400 pari envov numéro fr.001 cendrilon ayot date 2901/2019 69 rue natier 46 st fertéol 22000 92360 ile-de-france command esoa0t9 echeance 24/08/2019 ae designation prix unir ht montant grand brun escargot manger 4100.00 100.00 petit mariniére uniforme bleu 15.00 30.00 facile jouer accordéon 5.00 total 145.00 20.0 29.00 174.006 condition modalite paiement d0 15 jour caisse epargne iban fr12 1234 5678 swiftibic abcdfrp1xx facturer envoyer cendritlon ferréol lle-de-france are 1 . modalité dd swift / bic abcdfrp1xxx"
 
