b97b57f0071bf5550f6447d6208c131f2a896bf7
abenayoub <aymen.ba@gmail.com>
2024-10-22 04:38:42 +0200

    Add metrics export

--- steps/evaluate/eval.py
+++ steps/evaluate/eval.py
@@ -1,31 +1,23 @@
 #%%
 import pandas as pd
+import numpy as np
+import json
+import os
 import joblib 
 from imblearn.metrics import classification_report_imbalanced
 
-tfidf_path = '../../data/vectorizers/tfidf.joblib'
 model_path = '../../data/models/ovrc.joblib'
 X_test_path = '../../data/processed/test/X_test.joblib'
 y_test_path = '../../data/processed/test/y_test.joblib'
-X_train_path = '../../data/processed/train/X_train.joblib'
-y_train_path = '../../data/processed/train/y_train.joblib'
+metrics_path = '../../metrics/report.json'
+
 
 #%%
 def load_variables():
-     tfidf = joblib.load(tfidf_path)
      model = joblib.load(model_path)
      X_test = joblib.load(X_test_path)
-     X_train = joblib.load(X_train_path)
-     y_train = joblib.load(y_train_path)
      y_test = joblib.load(y_test_path)
-     return tfidf, model, X_test, y_test, X_train, y_train
-
-def transform_dataset(X_test):
-     return tfidf.transform(X_test['cleaned_text'])
-
-def predict(X_test):
-     y_pred = model.predict(X_test)
-     return y_pred
+     return model, X_test, y_test
 
 def get_accuracy():
      return model.score(X_test, y_test)
@@ -33,34 +25,43 @@ def get_accuracy():
 def get_confusion_matrix(y_test, y_pred):
      return pd.crosstab (y_test, y_pred, rownames=['Classe réelle'], colnames=['Classe Prédite'])
 
-def get_classification_report_imbalanced(y_test, y_pred):
-    report_classif_imbalanced = classification_report_imbalanced(y_test, y_pred, zero_division=0, output_dict=True)
-    df = pd.DataFrame(report_classif_imbalanced).transpose()
-    df = df.reset_index()
-    return df
+def convert_numpy_types(obj):
+    """
+    Recursively converts NumPy data types (e.g., np.float64, np.int64) to native Python types 
+    (e.g., float, int) within a given data structure (dict, list, or other).
+    """
+    if isinstance(obj, dict):
+        return {k: convert_numpy_types(v) for k, v in obj.items()}
+    elif isinstance(obj, np.float64) or isinstance(obj, np.float32):
+        return float(obj)
+    elif isinstance(obj, np.int64) or isinstance(obj, np.int32):
+        return int(obj)
+    elif isinstance(obj, list):
+        return [convert_numpy_types(item) for item in obj]
+    else:
+        return obj
+
+def get_metrics(y_test, y_pred):
+    report = classification_report_imbalanced(y_test, y_pred, output_dict=True)
+    report = convert_numpy_types(report)
+    json_report = json.dumps(report, indent=2)
+    print(json_report)
+    return json_report
+
+def save_json_metrics(metrics, file_path):
+    # Crée le répertoire s'il n'existe pas
+    directory = os.path.dirname(file_path)
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory)
+    # Sauvegarde les métriques au format JSON
+    with open(file_path, 'w') as json_file:
+        json.dump(metrics, json_file)
+    print(f"Report saved successfully to {file_path}.")
 
 #%%
-tfidf, model, X_test, y_test, X_train, y_train = load_variables()
-X_test = transform_dataset(X_test)
-y_pred = predict(X_test)
+model, X_test, y_test = load_variables()
+y_pred = model.predict(X_test)
 accuracy = get_accuracy()
 confusion_matrix = get_confusion_matrix(y_test, y_pred)
-report = get_classification_report_imbalanced(y_test, y_pred)
-print(type(report))
-#%%
-tfidf, model, X_test, y_test = load_variables()
-X_test = transform_dataset(X_test)
-y_pred = predict(X_test)
-accuracy = get_accuracy()
-confusion_matrix = get_confusion_matrix(y_test, y_pred)
-report = get_classification_report_imbalanced(y_test, y_pred)
-
-
-
-#%%
-import os
-os.getcwd()
-
-
-
-# %%
+metrics = get_metrics(y_test, y_pred)
+save_json_metrics(metrics, metrics_path)
