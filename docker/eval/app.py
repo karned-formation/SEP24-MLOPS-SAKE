001d6db9f38354682419db74d68568ba0808c1f9
abenayoub <aymen.ba@gmail.com>
2024-10-30 12:04:03 +0100

    Add Eval Docker

---
+++ docker/eval/app.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, HTTPException, Depends
+from eval import main
+import os
+
+MODEL_PATH = '/app/data/models/ovrc.joblib'
+X_TEST_PATH = '/app/data/processed/test/X_test.joblib'
+Y_TEST_PATH = '/app/data/processed/test/y_test.joblib'
+METRICS_DIR = '/app/metrics/'
+
+app = FastAPI()
+
+def get_paths():
+    paths = {
+        "model_path": MODEL_PATH,
+        "X_test_path": X_TEST_PATH,
+        "y_test_path": Y_TEST_PATH,
+        "metrics_dir": METRICS_DIR,
+    }
+
+    # Ensure the metrics directory exists
+    os.makedirs(paths["metrics_dir"], exist_ok=True)
+    return paths
+
+@app.get('/eval')
+def eval(paths: dict = Depends(get_paths)):
+    try:
+        # Run the main evaluation function
+        main(paths["model_path"], paths["X_test_path"], paths["y_test_path"], paths["metrics_dir"])
+        return {"message": f"Metrics saved successfully to {paths['metrics_dir']}"}
+    except FileNotFoundError as e:
+        # Handle missing files
+        raise HTTPException(status_code=404, detail=f"File not found: {e}")
+    except Exception as e:
+        # Handle any other exceptions
+        raise HTTPException(status_code=500, detail=f"An error occurred during evaluation: {e}")
---
+++ src/eval/eval.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+from imblearn.metrics import classification_report_imbalanced
+import json
+import os
+import joblib 
+
+model_path = '../../data/models/ovrc.joblib'
+X_test_path = '../../data/processed/test/X_test.joblib'
+y_test_path = '../../data/processed/test/y_test.joblib'
+metrics_dir = '../../metrics/'
+
+def load_variables(model_path, X_test_path, y_test_path):
+    """Load the model, X_test, and y_test from specified file paths."""
+    model = joblib.load(model_path)
+    X_test = joblib.load(X_test_path)
+    y_test = joblib.load(y_test_path)
+    return model, X_test, y_test
+
+def get_accuracy(model, X_test, y_test):
+    """Calculate and return model accuracy on the test data."""
+    return model.score(X_test, y_test)
+
+def get_confusion_matrix(y_test, y_pred):
+    """Generate a confusion matrix from true and predicted labels."""
+    return pd.crosstab(y_test, y_pred, rownames=['True Class'], colnames=['Predicted Class'])
+
+def convert_numpy_types(obj):
+    """Convert NumPy types in an object to native Python types recursively."""
+    if isinstance(obj, dict):
+        return {k: convert_numpy_types(v) for k, v in obj.items()}
+    elif isinstance(obj, np.float64) or isinstance(obj, np.float32):
+        return float(obj)
+    elif isinstance(obj, np.int64) or isinstance(obj, np.int32):
+        return int(obj)
+    elif isinstance(obj, list):
+        return [convert_numpy_types(item) for item in obj]
+    else:
+        return obj
+
+def get_metrics(model, X_test, y_test, y_pred):
+    """Compute classification metrics, convert types, and return as JSON."""
+    report = classification_report_imbalanced(y_test, y_pred, output_dict=True)
+    report['accuracy'] = get_accuracy(model, X_test, y_test)
+    report = convert_numpy_types(report)
+    json_report = json.dumps(report)
+    return json_report
+
+def save_json_metrics(metrics, metrics_dir):
+    """Save metrics as a JSON file at the specified file path."""
+    file_path = os.path.join(metrics_dir, "scores.json")
+    if metrics_dir and not os.path.exists(metrics_dir):
+        os.makedirs(metrics_dir)
+    with open(file_path, 'w') as json_file:
+        json.dump(metrics, json_file, indent=4)
+    print(f"Metrics saved successfully to {file_path}.")
+
+def save_confusion_matrix(confusion_matrix, metrics_dir):
+    """Save the confusion matrix to a JSON file."""
+    confusion_matrix_path = os.path.join(metrics_dir, "confusion_matrix.json")
+    confusion_matrix.to_json(confusion_matrix_path)
+    print(f"Confusion matrix saved successfully to {confusion_matrix_path}.")
+
+def main(model_path, X_test_path, y_test_path, metrics_dir):
+    """Run model prediction, calculate metrics, and save results."""
+    model, X_test, y_test = load_variables(model_path, X_test_path, y_test_path)
+    y_pred = model.predict(X_test)
+    confusion_matrix = get_confusion_matrix(y_test, y_pred)
+    metrics = get_metrics(model, X_test, y_test, y_pred)
+    save_json_metrics(metrics, metrics_dir)
+    save_confusion_matrix(confusion_matrix, metrics_dir)
+
+# Execute main function
+if __name__ == "__main__":
+    main(model_path, X_test_path, y_test_path)
