16a5004cb94cc34fe1da34be729382d95999314c
abenayoub <aymen.ba@gmail.com>
2024-11-05 23:36:31 +0100

    Utilisation de la nouvelle structure de dossiers pour le docker de Preprocessing

--- docker/preprocessing/app.py
+++ docker/preprocessing/app.py
@@ -3,17 +3,17 @@ import os
 from preprocessing import main
 
 # Paths
-clean_dataset_path = "/app/data/cleaned/cleaned_dataset.csv"
+clean_dir_path = "/app/data/cleaned_per_classes"
 
 app = FastAPI()
 @app.get("/process")
 async def process_data():
     # Check if the dataset exists
-    if not os.path.exists(clean_dataset_path):
+    if not os.path.exists(clean_dir_path):
         raise HTTPException(status_code=404, detail="Dataset not found")
 
     try:
-        main(clean_dataset_path)
+        main(clean_dir_path)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error during processing: {e}")
 
