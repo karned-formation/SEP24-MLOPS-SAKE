89e97d5ee3fc0e7e2fe0322dfd8d95b92da3b2d6
abenayoub <aymen.ba@gmail.com>
2024-12-11 16:05:38 +0100

    Improve Frontend

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -1,3 +1,4 @@
+
 import os
 import streamlit as st
 import requests
@@ -18,10 +19,22 @@ if st.button("Traiter"):
 
         with st.spinner("Envoi des données..."):
             response = requests.post(endpoint_url, data = data, files = files)
+        
+        r = response.json()
+        ref_is_unique = r['message'] != "Reference should be unique. Try again."
 
-        if response.status_code == 200:
-            st.success(f"Réponse du serveur : {response.text}")
-        else:
+        if response.status_code != 200:
             st.error(f"Erreur {response.status_code} : {response.text}")
+        elif ref_is_unique:
+            st.success(
+                f"Référence : {r['reference']} \n\n"
+                f"UUID : {r['uuid']} \n\n"
+                f"Prédiction : {r['prediction']} \n\n"
+                f"Message : {r['message']}"
+            )
+        else:
+            st.error("La référence doit être unique.")
     else:
         st.warning("Veuillez entrer une référence et sélectionner au moins un fichier.")
+
+
