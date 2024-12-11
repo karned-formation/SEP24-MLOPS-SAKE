6c417ae3298d42bc55ab08f0f43fcd10ae25c331
abenayoub <aymen.ba@gmail.com>
2024-12-11 16:06:08 +0100

    Add Background Taks to API

--- docker/orchestrator/app.py
+++ docker/orchestrator/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile, Form, BackgroundTasks
 from pathlib import Path
 from src.orchestrator.orchestrator import *
 from prometheus_fastapi_instrumentator import Instrumentator
@@ -9,17 +9,22 @@ Instrumentator().instrument(app).expose(app)
 # En attendant la mise en place de BDD
 database = {}
 
+def process_images(uuid: str):
+    print("Lancement de la Background Task")
+    prediction = main(uuid)
+    print(prediction)
+    return prediction
+
 # Endpoint to receive and save images
 @app.post("/predict")
-async def upload_images(
-    files: list[UploadFile] = File(...),
-    reference: str = Form(...)
-):
+async def upload_images(background_task: BackgroundTasks, 
+                        files: list[UploadFile] = File(...),
+                        reference: str = Form(...)
+                        ):
     if database.get(reference, 0):
         return {"message": "Reference should be unique. Try again."}
-    uuid, prediction = main(files)
+    uuid = save_images(files)
     database[reference] = uuid
     print(database) #TODO Delete
-    return {"message": "Files saved successfully", "reference": reference, "uuid": uuid, "prediction": prediction}
-
-    
+    background_task.add_task(process_images, uuid)
+    return {"message": "Files saved successfully", "reference": reference, "uuid": uuid}
