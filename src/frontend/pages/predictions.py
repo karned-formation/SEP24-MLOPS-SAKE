a1799804c46a4bf035aa049158821d738a827ee4
abenayoub <aymen.ba@gmail.com>
2024-12-18 21:49:07 +0100

    Add Raw Response & uuid

--- src/frontend/pages/predictions.py
+++ src/frontend/pages/predictions.py
@@ -4,8 +4,6 @@ from datetime import datetime
 import json
 import pandas as pd
 
-uuid = "blablabla"
-
 def convert_timestamp(timestamp):
     # Définir la langue pour les dates en français
     timestamp = float(timestamp)
@@ -27,48 +25,48 @@ def parse_response(pred):
     date = convert_timestamp(timestamp)
     n_images = pred['metadata']['n_files']
     status = pred['status']
+    uuid = pred['metadata']['uuid']
     prediction = process_prediction_dataframe(pred['prediction']) if status == 'COMPLETED' else None
-    return date, n_images, status, prediction
+    return date, n_images, status, uuid, prediction
 
 
 
 st.title("Afficher les prédictions")
 
 reference = st.text_input("Entrez une référence pour récupérer les prédictions:")
-
-if st.button("Display Predictions"):
+if st.button("Afficher les prédictions"):
     if reference:
         endpoint_url = f'http://localhost:8908/predict/{reference}'
+
         with st.spinner("Récupération des prédictions..."):
             response = requests.get(endpoint_url)
 
         if response.status_code != 200:
             st.error(f"Erreur {response.status_code} : {response.text}")
         else:
-            predictions = response.json()
-            if st.button("Display raw response"):
-                st.code(json.dumps(predictions, indent=2), language="json", line_numbers=True)
-            predictions = sorted(predictions, key=lambda x: x['metadata']['time'], reverse=True)
-            for pred in predictions:
-                date, n_images, status, prediction = parse_response(pred)
-                if status == 'COMPLETED':
-                    st.write(f"Prédiction du **{date}**")
-                    st.markdown(f"""
-                                - **Status :** {status}
-                                - **Nombre de fichiers :** {n_images}
-                                - **uuid :** {uuid}
-                                """)
-                    st.dataframe(prediction, hide_index=True)
-                else:
-                    st.write(f"Prédiction du {date} incluant {n_images} images")
-                    st.write(f"Status : {status}")
+            if len(response.json()):
+                st.json(response.json(), expanded=False)
+                # st.code(json.dumps(response.json(), indent=2), language="json", line_numbers=True)
                 st.write("---")
-
-            st.subheader("Prédictions :")
-            if isinstance(preds, list):
-                for i, pred in enumerate(preds, start=1):
-                    st.write(f"{i}. {pred}")
+                st.divider()
+                st.subheader("Prédictions :", divider=True)
+                predictions = sorted(response.json(), key=lambda x: x['metadata']['time'], reverse=True)
+                for pred in predictions:
+                    date, n_images, status, uuid, prediction = parse_response(pred)
+                    if status == 'COMPLETED':
+                        st.write(f"Prédiction du **{date}**")
+                        st.markdown(f"""
+                                    - **Status :** {status}
+                                    - **Nombre de fichiers :** {n_images}
+                                    - **uuid :** {uuid}
+                                    """)
+                        st.dataframe(prediction, hide_index=False, use_container_width=True)
+                    else:
+                        st.write(f"Prédiction du {date} incluant {n_images} images")
+                        st.write(f"Status : {status}")
+                    st.write("---")
             else:
-                st.write(preds)
+                st.warning("Référence invalide")
+
     else:
         st.warning("Veuillez entrer une référence.")
