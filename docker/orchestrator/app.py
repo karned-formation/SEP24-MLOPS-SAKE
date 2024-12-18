97a1603a47e894185f05f0f74a4bc8c3f997d16a
abenayoub <aymen.ba@gmail.com>
2024-12-18 13:39:16 +0100

    Add Predict/{reference} endpoint

--- docker/orchestrator/app.py
+++ docker/orchestrator/app.py
@@ -2,18 +2,25 @@ from fastapi import FastAPI, File, UploadFile, Form, BackgroundTasks
 from pathlib import Path
 from src.orchestrator.orchestrator import *
 from prometheus_fastapi_instrumentator import Instrumentator
+from collections import defaultdict
+from src.custom_logger import logger
+import time
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
 
-# En attendant la mise en place de BDD
+# En attendant la mise en place d'une vraie BDD
+reference_uuid_map = defaultdict(list)
 database = {}
+ 
 
 def process_images(uuid: str):
-    print("Lancement de la Background Task")
-    prediction = main(uuid)
-    print(prediction)
-    return prediction
+    logger.info("Lancement de la Background Task")
+    database[uuid]['status'] = 'IN_PROGRESS'
+    database[uuid]['prediction'] = main(uuid)
+    logger.info("Prediction ajoutée à la databse")
+    database[uuid]['status'] = 'COMPLETED'
+
 
 # Endpoint to receive and save images
 @app.post("/predict")
@@ -21,10 +28,26 @@ async def upload_images(background_task: BackgroundTasks,
                         files: list[UploadFile] = File(...),
                         reference: str = Form(...)
                         ):
-    if database.get(reference, 0):
-        return {"message": "Reference should be unique. Try again."}
     uuid = save_images(files)
-    database[reference] = uuid
-    print(database) #TODO Delete
+    database[uuid] = {
+        'status': 'PENDING',
+        'metadata': None ,
+        'prediction': None
+    }
+
+    metadata = dict()
+    metadata['n_files'] = len(files)
+    metadata['time'] = time.time()
+    metadata['uuid'] = uuid
+    database[uuid]['metadata'] = metadata
+    reference_uuid_map[reference].append(uuid)
+    reference_uuid_map[uuid].append(uuid)
     background_task.add_task(process_images, uuid)
-    return {"message": "Files saved successfully", "reference": reference, "uuid": uuid}
+    return {"message": "Files saved successfully", "reference": reference, "uuid": uuid, "len": metadata['n_files'], "time": metadata['time']}
+
+@app.get('/predict/{reference}')
+def get_prediction(reference):
+    predictions = []
+    for uuid in reference_uuid_map[reference]:
+        predictions.append(database[uuid])
+    return predictions 
