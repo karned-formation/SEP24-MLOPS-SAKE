61e6cda437a20fe393c61ad411fea19207e7a6de
abenayoub <aymen.ba@gmail.com>
2024-12-26 23:06:29 +0100

    Holidays Save

--- src/frontend/pages/Prediction_Results.py
+++ src/frontend/pages/Prediction_Results.py
@@ -1,8 +1,11 @@
+# TEST
 import streamlit as st
 import requests
 from datetime import datetime
 import json
 import pandas as pd
+
+st.set_page_config(page_title="Dashboard Page")
 
 def convert_timestamp(timestamp):
     # Définir la langue pour les dates en français
@@ -16,8 +19,9 @@ def process_prediction_dataframe(prediction):
     prediction = json.loads(prediction)
     df = pd.DataFrame(prediction).T
     df.index = df.index.str.split("/").str[-1]
-    df = df.reset_index()
-    df.rename(columns={'index': 'filename'}, inplace=True)
+    df.index.name = 'Filename'
+    # df = df.reset_index()
+    # df.rename(columns={'index': 'filename'}, inplace=True)
     return df
 
 def parse_response(pred):
@@ -34,7 +38,7 @@ def parse_response(pred):
 st.title("Afficher les prédictions")
 
 reference = st.text_input("Entrez une référence pour récupérer les prédictions:")
-if st.button("Afficher les prédictions"):
+if st.button("Affichage des prédictions"):
     if reference:
         endpoint_url = f'http://localhost:8908/predict/{reference}'
 
@@ -45,11 +49,10 @@ if st.button("Afficher les prédictions"):
             st.error(f"Erreur {response.status_code} : {response.text}")
         else:
             if len(response.json()):
-                st.json(response.json(), expanded=False)
-                # st.code(json.dumps(response.json(), indent=2), language="json", line_numbers=True)
+                with st.expander("View JSON Response", expanded=False, icon=":material/file_json:"):
+                    st.json(response.json())
                 st.write("---")
-                st.divider()
-                st.subheader("Prédictions :", divider=True)
+                # st.subheader("Prédictions :")
                 predictions = sorted(response.json(), key=lambda x: x['metadata']['time'], reverse=True)
                 for pred in predictions:
                     date, n_images, status, uuid, prediction = parse_response(pred)
@@ -60,7 +63,25 @@ if st.button("Afficher les prédictions"):
                                     - **Nombre de fichiers :** {n_images}
                                     - **uuid :** {uuid}
                                     """)
-                        st.dataframe(prediction, hide_index=False, use_container_width=True)
+                        # st.dataframe(prediction, hide_index=False, use_container_width=True)
+                        st.dataframe(
+                            prediction.style\
+                            .format("{:.2%}")\
+                            .highlight_max(axis=1, color='green'),
+                            hide_index=False, 
+                            use_container_width=True,
+                            column_config={
+                                    "Prob_class_0": st.column_config.NumberColumn(
+                                        "Probabilité Facture",
+                                    ),
+                                    "Prob_class_1": st.column_config.NumberColumn(
+                                        "Probabilité ID_Piece",
+                                    ),
+                                    "Prob_class_2": st.column_config.NumberColumn(
+                                        "Probabilité Resume",
+                                    ),
+                                },
+                        )
                     else:
                         st.write(f"Prédiction du {date} incluant {n_images} images")
                         st.write(f"Status : {status}")
