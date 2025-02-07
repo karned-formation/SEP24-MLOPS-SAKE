570ea6ad6b596f27868d5288ee47b8fd0ec230d6
Killian KOPP <killiankopp@gmail.com>
2025-02-07 11:24:01 +0100

    docker etl

--- docker/etl/app.py
+++ docker/etl/app.py
@@ -1,28 +1,77 @@
 from fastapi import FastAPI, HTTPException
-from starlette.responses import PlainTextResponse
 from prometheus_fastapi_instrumentator import Instrumentator
-from src.data.ingest_etl import ingest_train, ingest_prediction
-from src.data.clean_etl import clean_train, clean_prediction
+from starlette.responses import PlainTextResponse
 
-app = FastAPI()
-Instrumentator().instrument(app).expose(app)
+from src.data.clean_etl import clean_prediction, clean_train
+from src.data.ingest_etl import ingest_prediction, ingest_train
+from pydantic import BaseModel
 
-@app.post("/ingest", response_class = PlainTextResponse)
-def ingest(prediction_folder:str = None):
+app = FastAPI(
+    title="ETL",
+    description="API Etract Transform Load.",
+    version="1.0.0"
+)
+Instrumentator().instrument(app).expose(
+    app=app,
+    endpoint="/etl/metrics"
+)
+
+
+class InputIngest(BaseModel):
+    uri: str
+
+
+class InputClean(BaseModel):
+    uri: str
+
+
+class OutputIngest(BaseModel):
+    data: str
+
+
+class OutputClean(BaseModel):
+    data: str
+
+
+@app.post(
+    path="/etl/ingest",
+    response_class=PlainTextResponse,
+    tags=["ETL"])
+def ingest( input_data: InputIngest ):
     try:
-        if prediction_folder:
-            ingest_prediction(prediction_folder)
-        else:
-            ingest_train()
+        ingest_prediction(input_data.uri)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.post("/clean", response_class = PlainTextResponse)
-def clean(prediction_folder:str = None):
+
+@app.post(
+    path="/etl/clean",
+    response_class=PlainTextResponse,
+    tags=["ETL : train"])
+def clean( input_data: InputClean ):
     try:
-        if prediction_folder:
-            clean_prediction(prediction_folder)
-        else :
-            clean_train()
+        clean_prediction(input_data.uri)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post(
+    path="/etl/ingest/train",
+    response_class=PlainTextResponse,
+    tags=["ETL : train"])
+def ingest():
+    try:
+        ingest_train()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post(
+    path="/etl/clean/train",
+    response_class=PlainTextResponse,
+    tags=["ETL"])
+def clean():
+    try:
+        clean_train()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
