893c79f54ffed396b48fbf04281f6304bfbe8599
Killian KOPP <killiankopp@gmail.com>
2025-02-09 19:10:40 +0100

    WIP frontend - hard link

--- docker/frontend/src/utils/files.py
+++ docker/frontend/src/utils/files.py
@@ -1,1 +1,10 @@
-../../../../src/utils/files.py
+import base64
+
+
+def encode_files(files):
+    files_base64 = []
+    for uploaded_file in files:
+        file_content = uploaded_file.read()
+        encoded_string = base64.b64encode(file_content).decode('utf-8')
+        files_base64.append(encoded_string)
+    return files_base64
