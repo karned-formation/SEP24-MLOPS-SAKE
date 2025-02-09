961c5fea27a3a69797d708ca698a3334781b4481
Killian KOPP <killiankopp@gmail.com>
2025-02-09 18:34:23 +0100

    WIP clean - perso docs swagger

--- docker/clean_text/app.py
+++ docker/clean_text/app.py
@@ -4,10 +4,20 @@ from prometheus_fastapi_instrumentator import Instrumentator
 
 from src.data.clean_text import tokenize_data
 
-app = FastAPI()
+app = FastAPI(
+    title="Clean",
+    description="API Clean text.",
+    version="1.0.0"
+)
+Instrumentator().instrument(app).expose(
+    app=app,
+    endpoint="/clean/metrics"
+)
 
-Instrumentator().instrument(app).expose(app)
-
-@app.post("/clean", response_class = PlainTextResponse)
+@app.post(
+    path="/clean",
+    response_class = PlainTextResponse,
+    tags=["Clean"]
+)
 def clean(text: str):
     return tokenize_data(text)
