7ddd46e38c7edddf505c48fdca04dd6d3bee3d57
Belwen <sarah@BOOK-JLC4EUKSB0.>
2025-02-09 18:06:29 +0100

    session state for training streamlit and correct params for new ocr service on port 80

--- src/streamlit/admin-frontend.py
+++ src/streamlit/admin-frontend.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from urllib.parse import quote
 import os
+from streamlit_extras.let_it_rain import rain
 
 # Set page configuration
 st.set_page_config(
@@ -15,6 +16,22 @@ st.set_page_config(
 )
 
 BACKEND_URL = os.getenv("ADMIN_BACKEND_ENDPOINT_ADMIN_BACKEND")
+
+# Handle session state
+if "selected_folder" not in st.session_state:
+    st.session_state.selected_folder = None
+
+if "training_result" not in st.session_state:
+    st.session_state.training_result = None
+
+if 'training_status' not in st.session_state:
+    st.session_state.training_status = None
+
+if 'mlflow_runs' not in st.session_state:
+    st.session_state.mlflow_runs = None
+
+if 'selected_runs' not in st.session_state:
+    st.session_state.selected_runs = pd.DataFrame()
 
 def main():    
     # Sidebar for navigation
@@ -27,78 +44,65 @@ def main():
     else:
         show_version_management()
 
-
-
 def show_image_management():
     st.header("Image Management")
     
-    # Get folders and images
     response = requests.get(f"{BACKEND_URL}/get_images")
     if response.status_code == 200:
-        folders_data = response.json()
+        st.session_state.folders_data = response.json()
+    
+    if "folders_data" in st.session_state:
+        folder = st.selectbox("Select Folder", list(st.session_state.folders_data.keys()),
+                              index=list(st.session_state.folders_data.keys()).index(st.session_state.selected_folder) if st.session_state.selected_folder in st.session_state.folders_data else 0)
+        st.session_state.selected_folder = folder
         
-        # Select folder
-        folder = st.selectbox("Select Folder", list(folders_data.keys()))
-        
-        # Upload new images
         uploaded_file = st.file_uploader(f"Add image to {folder}", type=["jpg", "png"])
         if uploaded_file:
             add_image(folder, uploaded_file)
-
-        # Display images in grid
-        if folder in folders_data:
+        
+        if folder in st.session_state.folders_data:
             cols = st.columns(6)
-            for idx, img_data in enumerate(folders_data[folder]):
+            for idx, img_data in enumerate(st.session_state.folders_data[folder]):
                 with cols[idx % 6]:
                     st.image(img_data["thumbnail"], caption=img_data["name"])
                     if st.button(f"Delete {img_data['name']}", key=f"del_{folder}_{idx}"):
                         delete_image(folder, img_data["name"])
-        
 
 def show_training_page():
     st.header("Training Management")
-    # Training button
+    
     if st.button("Start Training"):
         with st.spinner("Training in progress..."):
             response = requests.post(f"{BACKEND_URL}/train")
             if response.status_code == 200:
-                result = json.loads(response.content)                
+                st.session_state.training_result = json.loads(response.content)
                 st.success("Training completed!")
-                
-                # Create two columns
-                col1, col2 = st.columns(2)
-                
-                # Left column with scores
-                with col1:
-                    st.subheader("Scores")
-                    scores = json.loads(result['scores'].replace("'", '"'))
-                    st.write(f"Overall Accuracy: {scores['accuracy']:.2%}")
-                    st.write(f"Overall f1 score: {scores['avg_f1']:.2%}")
-                    st.write(f"Overall Recall: {scores['avg_rec']:.2%}")
-
-                # Right column with confusion matrix
-                with col2:
-                    matrix_dict = json.loads(result['confusion_matrix'].replace("'", '"')) 
-                    matrix = pd.DataFrame.from_dict(matrix_dict)
-                    
-                    # Convert matplotlib plot to streamlit
-                    fig = plt.figure(figsize=(5,4))
-                    sns.heatmap(matrix, annot=True, cmap='Blues', fmt='d', 
-                              xticklabels=['Facture', 'Identité', 'CV'],
-                              yticklabels=['Facture', 'Identité', 'CV'],
-                              annot_kws={"fontsize":16})
-                    plt.title('Confusion Matrix', fontsize=20)
-                    plt.xlabel('Predicted', fontsize=12)
-                    plt.ylabel('Actual', fontsize=12)
-                    plt.xticks(fontsize=16)
-                    plt.yticks(fontsize=16)
-                    
-                    # Display the plot in Streamlit
-                    st.pyplot(fig)
+                rain(emoji="🎉", font_size=60, falling_speed=5, animation_length=5)
             else:
-                st.error(response.content)
-
-    # Model registration
+                st.error("Training failed!")
+    
+    if st.session_state.training_result:
+        result = st.session_state.training_result
+        
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.subheader("Scores")
+            scores = json.loads(result['scores'].replace("'", '"'))
+            st.write(f"Overall Accuracy: {scores['accuracy']:.2%}")
+            st.write(f"Overall f1 score: {scores['avg_f1']:.2%}")
+            st.write(f"Overall Recall: {scores['avg_rec']:.2%}")
+        
+        with col2:
+            matrix_dict = json.loads(result['confusion_matrix'].replace("'", '"'))
+            matrix = pd.DataFrame.from_dict(matrix_dict)
+            fig = plt.figure(figsize=(5,4))
+            sns.heatmap(matrix, annot=True, cmap='Blues', fmt='d', 
+                        xticklabels=['Facture', 'Identité', 'CV'],
+                        yticklabels=['Facture', 'Identité', 'CV'],
+                        annot_kws={"fontsize":16})
+            plt.title('Confusion Matrix', fontsize=18)
+            st.pyplot(fig)
+    
     if st.button("Register Current Model to S3"):
         with st.spinner("Registering model..."):
             response = requests.post(f"{BACKEND_URL}/registermodel")
@@ -107,31 +111,23 @@ def show_training_page():
             else:
                 st.error("Model registration failed!")
 
-
 def show_version_management():
     st.header("Version Management")
-
-    # Display MLflow runs
     response = requests.get(f"{BACKEND_URL}/getmlflowruns")
     if response.status_code == 200:
-        runs = response.json()
-        df = pd.DataFrame(json.loads(runs))
-        display_mlflow_runs(df)
+        st.session_state.mlflow_runs = pd.DataFrame(json.loads(response.json()))
+    
+    if st.session_state.mlflow_runs is not None:
+        st.session_state.selected_runs = display_mlflow_runs(st.session_state.mlflow_runs)
 
 def display_mlflow_runs(runs_df):
-    """
-    Display MLflow runs with selection and revert functionality
-    
-    Args:
-        runs_df (pd.DataFrame): DataFrame of MLflow runs
-    """
     runs_df['start_time'] = pd.to_datetime(runs_df['start_time'], unit='ms')
     runs_df['end_time'] = pd.to_datetime(runs_df['end_time'], unit='ms')
-
-    # Add a selection column
     runs_df['Select'] = False
     
-    # Display the dataframe with selection
+    if not st.session_state.selected_runs.empty:
+        runs_df.update(st.session_state.selected_runs)
+    
     edited_df = st.data_editor(
         runs_df, 
         column_config={
@@ -142,25 +138,18 @@ def display_mlflow_runs(runs_df):
             )
         },
         disabled=[
-            'tags.mlflow.runName', 
-            'start_time', 
-            'end_time', 
-            'status', 
-            'metrics.accuracy', 
-            'metrics.f1_score', 
-            'tags.commit_hash'
+            'tags.mlflow.runName', 'start_time', 'end_time', 'status', 
+            'metrics.accuracy', 'metrics.f1_score', 'tags.commit_hash'
         ],
         hide_index=True
     )
     
-    # Count selected runs
     selected_runs = edited_df[edited_df['Select']]
+    st.session_state.selected_runs = selected_runs
     
-    # Validate selection
     if len(selected_runs) > 1:
         st.warning("Please select only one run.")
     
-    # Revert button
     if len(selected_runs) == 1:
         selected_run = selected_runs.iloc[0]
         commit_hash = selected_run['commit_hash']
@@ -177,41 +166,26 @@ def display_mlflow_runs(runs_df):
     
 
 def delete_image(folder, image_name):
-    headers = {"Content-Type": "application/json"}  # Retain headers if required
-    # Construct the query parameters
-    query_params = {
-        "folder": folder,
-        "image_name": image_name
-    }
-    # Encode query parameters into the URL
     encoded_url = f"{BACKEND_URL}/deleteimage?folder={quote(folder)}&image_name={quote(image_name)}"
-
     try:
-        response = requests.post(encoded_url, headers=headers)
-        
+        response = requests.post(encoded_url)
         if response.status_code == 200:
             st.success(f"Deleted {image_name}")
             st.rerun()
         else:
-            # Log full response details for debugging
-            st.error(f"Failed to delete image: {image_name}")
-            st.error(f"Response status: {response.status_code}")
-            st.error(f"Response body: {response.text}")
-    
+            st.error(f"Failed to delete {image_name}")
     except requests.RequestException as e:
-        # Handle connection errors
         st.error(f"Request failed: {e}")
 
 def add_image(folder, file):
-    files = {"file": file}
-    response = requests.post(
-        f"{BACKEND_URL}/addimage",
-        files=files,
-        data={"folder": folder}
-    )
+    response = requests.post(f"{BACKEND_URL}/addimage", files={"file": file}, data={"folder": folder})
     if response.status_code == 200:
         st.success("Image uploaded successfully!")
-        st.rerun()
+
+        # Update session state, no manual rerun
+        response = requests.get(f"{BACKEND_URL}/get_images")
+        if response.status_code == 200:
+            st.session_state.folders_data = response.json()
     else:
         st.error("Failed to upload image")
 
