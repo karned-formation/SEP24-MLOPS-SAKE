fa0374683c46acdfb9d9128df9d87cc8219be46e
abenayoub <aymen.ba@gmail.com>
2025-02-17 20:58:28 +0100

    Keep filename

--- docker/frontend/src/utils/files.py
+++ docker/frontend/src/utils/files.py
@@ -6,5 +6,8 @@ def encode_files(files):
     for uploaded_file in files:
         file_content = uploaded_file.read()
         encoded_string = base64.b64encode(file_content).decode('utf-8')
-        files_base64.append(encoded_string)
+        files_base64.append({
+            "name": uploaded_file.name,
+            "content": encoded_string
+        })
     return files_base64
